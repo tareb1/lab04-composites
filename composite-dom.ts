ebbe5122aa006add6d1d7d129174a49cda47c7f2
Rebecca <tareb1@uw.edu>
2017-01-24 11:13:17 -0800

    lab participation

--- composite-dom.ts
+++ composite-dom.ts
@@ -2,3 +2,43 @@ interface IDomElement {
   print();
 }
 
+class DomElement implements IDomElement{
+  elementName : string;
+  textContent: string;
+  elements: IDomElement[];
+
+  constructor(elementName : string, textContent?: string) {
+    this.elementName = elementName;
+    this.elements = [];
+    this.textContent = textContent;
+  }
+
+  add(element: IDomElement) {
+    this.elements.push(element);
+  }
+
+  print() {
+    console.log(`<${this.elementName}>`);
+    if (this.textContent) {
+      console.log(this.textContent);
+    }
+    for (let element of this.elements) {
+      element.print();
+    }
+    console.log(`</${this.elementName}>`);
+  }
+}
+
+
+
+
+class TextNode implements IDomElement {
+  private text : string;
+  constructor(text : string) {
+    this.text = text;
+  }
+  print() {
+    console.log(this.text);
+  }
+
+}
